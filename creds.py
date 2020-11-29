a32d3005c49902367fc6dbb840186611a8eea6fa
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-11-29 18:38:23 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -8,4 +8,4 @@ class Credentials:
     BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
     API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
     API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
-    START_MESSAGE = os.getenv("START_MESSAGE") #Not Mandatory
+    START_MESSAGE = os.getenv("START_MESSAGE",None) #Not Mandatory
