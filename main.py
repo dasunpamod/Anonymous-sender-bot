c5412dff7667395ed468700fea44a3cf8c8a498e
ProThinkerGang <71463576+ProThinkerGang@users.noreply.github.com>
2020-11-29 17:32:11 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from telethon.sync import TelegramClient, events, Button
+from telethon import errors
+from telethon.tl.types import InputPeerChat
+from telethon.errors import FloodWaitError
+from telethon.tl.types import ChatEmpty
+import os
+import uuid
+import shutil
+import asyncio
+import logging
+logging.basicConfig(level=logging.INFO)
+
+from creds import Credentials
+
+client = TelegramClient('Telethon Anonymous Bot',
+                    api_id = Credentials.API_ID,
+                    api_hash=Credentials.API_HASH).start(bot_token=Credentials.BOT_TOKEN)
+
+DEFAULT_START = ("Hi, I am Anonymous Sender Bot.\n\n"
+                 "Just Forward me Some messages\n"
+                 "or media and I will Anonymize\n"
+                 "the sender.\n\n"
+                 "**Note -**\n"
+                 "__We Dont Promote Circulation\n__"
+                 "__of Copyright Contents. This Bot is\n__"
+                 "__Created for Educational Purpose\n__"
+                 "__Only !!\n\n__"
+                 "Please Support The Developer\n"
+                 "By Joining the Support Channel👇👇")
+
+if Credentials.START_MESSAGE is None:
+  START_MESSAGE = DEFAULT_START
+else:
+  START_MESSAGE = START_MESSAGE
+  
+@client.on(events.NewMessage)
+async def startmessage(event):
+  try:
+    if '/start' in event.raw_text:
+      ok = event.chat_id
+      await client.send_message(event.chat_id,f"{START_MESSAGE}",
+                                buttons=[[Button.url("✤ SUPPORT CHANNEL ✤","t.me/Prothinkergang")]])                                                                 
+    if event.message.media:
+      await client.send_message(event.chat_id,file=event.message.media)
+    else:
+      await client.send_message(event.chat_id,event.message)
+  except FloodWaitError as e:
+    pass
+    
+
+with client:
+  client.run_until_disconnected() 
